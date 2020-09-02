5ec5f61597e59884303cbd9f329e7f3cf7fc82c6
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2020-09-02 10:40:40 -0300

    lenet5 modificada, maxpooling filtros 32 64, dropout

--- 01_CNN.py
+++ 01_CNN.py
@@ -273,9 +273,9 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 ## LeNet-5 - modificada com duas camadas de dropout e maxpooling e filtros 16 32
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
