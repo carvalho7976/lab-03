d3724ebcde36715dbadac13b925fc713cb8be594
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2020-09-03 16:30:14 -0300

    test with new images parameters, save

--- 01_CNN.py
+++ 01_CNN.py
@@ -327,8 +327,7 @@ model.compile(metrics=['accuracy'], loss=keras.losses.categorical_crossentropy, 
 print("Treinando....")
 
 #history = model.fit(x=x_train, y=y_train, batch_size=batch_size,epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
-#history = model.fit(x=aug.flow(x_train,y_train,batch_size=32, save_format='jpg', save_to_dir=save_path),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
-history = model.fit(x=aug.flow(x_train,y_train,batch_size=32),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
+history = model.fit(x=aug.flow(x_train,y_train,batch_size=32, save_format='jpg', save_to_dir=save_path),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
 
 score = model.evaluate(x_test, y_test, verbose=1)
 print ('\n----------------------------------------------------\n')
