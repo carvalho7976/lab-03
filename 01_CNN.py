3a830422cf72b1522ac48f87429d2acf9150c04a
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2020-09-03 10:10:49 -0300

    change data aumentation specs

--- 01_CNN.py
+++ 01_CNN.py
@@ -312,8 +312,11 @@ print ('----------------------------------------------------')
 # # **3. Configurando e treinando a CNN**
 #Data aumentation
 aug = ImageDataGenerator(
-		rotation_range=0.02,
-		brightness_range=[0.01,0.3],
+		featurewise_center=True, 
+		featurewise_std_normalization=True,
+		zca_whitening=True,
+		rotation_range=0.3,
+		brightness_range=[0.1,0.5],
 		fill_mode="nearest")
 
 ## Configures the model for training
@@ -324,7 +327,7 @@ model.compile(metrics=['accuracy'], loss=keras.losses.categorical_crossentropy, 
 print("Treinando....")
 
 #history = model.fit(x=x_train, y=y_train, batch_size=batch_size,epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
-history = model.fit(x=aug.flow(x_train,y_train,batch_size=4, save_format='jpg', save_to_dir=save_path),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
+history = model.fit(x=aug.flow(x_train,y_train,batch_size=2, save_format='jpg', save_to_dir=save_path),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
 
 score = model.evaluate(x_test, y_test, verbose=1)
 print ('\n----------------------------------------------------\n')
