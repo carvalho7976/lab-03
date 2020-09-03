25a8d9e6c3a5387ae0e2f361b16ed0c7dee57379
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2020-09-03 10:51:37 -0300

    change data aumentation

--- 01_CNN.py
+++ 01_CNN.py
@@ -327,7 +327,7 @@ model.compile(metrics=['accuracy'], loss=keras.losses.categorical_crossentropy, 
 print("Treinando....")
 
 #history = model.fit(x=x_train, y=y_train, batch_size=batch_size,epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
-history = model.fit(x=aug.flow(x_train,y_train,batch_size=2, save_format='jpg', save_to_dir=save_path),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
+history = model.fit(x=aug.flow(x_train,y_train,batch_size=batch_size, save_format='jpg', save_to_dir=save_path),epochs=n_epochs, verbose=0, validation_data=(x_test, y_test))
 
 score = model.evaluate(x_test, y_test, verbose=1)
 print ('\n----------------------------------------------------\n')
